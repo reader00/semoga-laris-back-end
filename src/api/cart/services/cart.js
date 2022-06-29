0a22da56b9cc2e178ff13a9d038375c646cc411a
reader00 <qoryamanahputra0708@gmail.com>
2022-06-29 18:25:58 +0700

    add addItem service on api::cart.cart

--- src/api/cart/services/cart.js
+++ src/api/cart/services/cart.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * cart service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::cart.cart');
+module.exports = createCoreService("api::cart.cart", ({ strapi }) => ({
+    async addItem(cart_id, { count, total }) {
+        // get cart
+        let cart = await strapi.entityService.findOne(
+            "api::cart.cart",
+            cart_id,
+            {
+                fields: ["count", "total"],
+            }
+        );
+
+        count = Number(count) + Number(cart.count);
+        total = Number(total) + Number(cart.total);
+
+        await strapi.entityService.update("api::cart.cart", cart_id, {
+            data: {
+                count,
+                total,
+            },
+        });
+    },
+}));
