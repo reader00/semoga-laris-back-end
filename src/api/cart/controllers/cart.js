223807b49ef495b684c9646aceea5dfa928dddaa
reader00 <qoryamanahputra0708@gmail.com>
2022-06-30 15:43:10 +0700

    add comment and delete unneccessary data

--- src/api/cart/controllers/cart.js
+++ src/api/cart/controllers/cart.js
@@ -8,6 +8,7 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::cart.cart", ({ strapi }) => ({
     async find(ctx) {
+        // Get and set neccessary data
         const owner = ctx.state.user.id;
         ctx.request.query = {
             ...ctx.request.query,
@@ -16,15 +17,20 @@ module.exports = createCoreController("api::cart.cart", ({ strapi }) => ({
                 is_checked_out: false,
             },
         };
+
+        // Find original data
         let { data, meta } = await super.find(ctx);
 
+        // Check if there is no cart yet
         if (data.length === 0) {
+            // Create new cart
             await strapi.entityService.create("api::cart.cart", {
                 data: {
                     owner,
                 },
             });
 
+            // Get cart
             const cart = await super.find(ctx);
             data = cart.data;
             meta = cart.meta;
@@ -34,6 +40,7 @@ module.exports = createCoreController("api::cart.cart", ({ strapi }) => ({
     },
 
     async findOne(ctx) {
+        // Get and set neccessary data
         const owner = ctx.state.user.id;
         ctx.query = {
             ...ctx.query,
@@ -44,18 +51,17 @@ module.exports = createCoreController("api::cart.cart", ({ strapi }) => ({
             },
         };
 
-        // const data = await super.findOne(ctx);
+        // Get original data
         const { data, meta } = await super.findOne(ctx);
 
-        strapi.log.debug(JSON.stringify(data));
-
+        // Check ownership
         if (owner !== data.attributes.owner.data.id) {
             ctx.forbidden("Forbidden Error");
         }
 
+        // Delete attribute owner
         delete data.attributes.owner;
 
-        // if()
         return { data, meta };
     },
 }));
