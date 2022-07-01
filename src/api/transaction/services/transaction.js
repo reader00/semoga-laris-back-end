36531e0adc31671f44ea84705dc4c036ebfc3c7f
reader00 <qoryamanahputra0708@gmail.com>
2022-07-01 17:01:53 +0700

    create service to create transaction detail

--- src/api/transaction/services/transaction.js
+++ src/api/transaction/services/transaction.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * transaction service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::transaction.transaction');
+module.exports = createCoreService(
+    "api::transaction.transaction",
+    ({ strapi }) => ({
+        async createTransactionDetail(transactionId, ctx) {
+            ctx.request.body.data.transaction = transactionId;
+
+            const transactionDetail = await strapi
+                .controller("api::transaction-detail.transaction-detail")
+                .create(ctx);
+            strapi.log.debug(JSON.stringify(transactionDetail));
+        },
+    })
+);
