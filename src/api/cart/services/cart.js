a7b0dfb1550003f56d9d6a31033c80598f4ff569
reader00 <qoryamanahputra0708@gmail.com>
2022-07-02 07:51:18 +0700

    add shipping_fee system

--- src/api/cart/services/cart.js
+++ src/api/cart/services/cart.js
@@ -10,7 +10,9 @@ module.exports = createCoreService("api::cart.cart", ({ strapi }) => ({
     async updateValues(cartId) {
         // get cart
         let total = 0,
-            count = 0;
+            count = 0,
+            productPrice = 0,
+            shippingFee = 40000;
 
         let cart = await strapi.entityService.findOne(
             "api::cart.cart",
@@ -34,13 +36,17 @@ module.exports = createCoreService("api::cart.cart", ({ strapi }) => ({
             const itemPrice = item.product.price;
 
             count += parseInt(itemCount);
-            total += parseInt(itemCount) * parseInt(itemPrice);
+            productPrice += parseInt(itemCount) * parseInt(itemPrice);
+            shippingFee += 10000;
         }
+        total = productPrice + shippingFee;
 
         await strapi.entityService.update("api::cart.cart", cartId, {
             data: {
                 count,
                 total,
+                shipping_fee: shippingFee,
+                products_price: productPrice,
             },
         });
     },
